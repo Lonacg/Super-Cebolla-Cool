f548568f6183d5bd0ff9054a3e5b04282c513222
Robert Silva <alsival@hotmail.com>
2022-04-17 13:40:35 +0200

    Limpieza, salto y añadido variable directionDrag

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     public float maxSpeed;
     public float acceleration;
     public float drag;
+    public float directionDrag;
     private float currSpeed;
 
     private Vector3 lastDirection;
@@ -32,6 +33,7 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
+        JumpControl();
         CustomGravity();
     }
 
@@ -43,121 +45,63 @@ public class Player : MonoBehaviour
     private void MovementControl()
     {
         Vector3 direction = GetDirection('x');
-        //Vector3 direction = GetDirection();
         bool shiftKey = Keyboard.current.shiftKey.isPressed;
 
-        // Incercia
         if (direction.x != 0)
         {
-            // Aceleración
             if (shiftKey)
             {
+                // Aceleración
                 if (currSpeed < maxSpeed)
                     currSpeed += acceleration * Time.deltaTime;
             }
-            else 
+            else
                 currSpeed = speed;
 
-
-            // Inercia al cambiar de dirección
+            // Inercia al cambio de dirección
             if (direction != lastDirection)
-                currSpeed *= 0.5f; 
+                currSpeed *= directionDrag; 
 
             lastDirection = direction;
         }
-        // Inercia al detenerse
         else 
+            // Inercia al detenerse
             currSpeed *= drag;
     
         transform.Translate(lastDirection * currSpeed * Time.deltaTime);
     }
 
-    private void AccelerationControl()
-    {
-
-    }
-
     private void JumpControl()
     {
-
+        Vector3 direction = GetDirection('y');
+        if (rigidbody.velocity.y == 0)
+        {
+            rigidbody.AddForce(
+                direction * jumpForce * Time.fixedDeltaTime, 
+                ForceMode.Impulse
+            );
+        }
     }
 
-    
     private Vector3 GetDirection(char axis)
     {
         Vector3 direction = Vector3.zero;
+
         bool rightPressed= Keyboard.current.rightArrowKey.isPressed;
         bool leftPressed= Keyboard.current.leftArrowKey.isPressed;
         bool upPressed= Keyboard.current.upArrowKey.isPressed;
+
         switch (axis)
         {
             case 'x':
-                if (rightPressed && !leftPressed)   direction.x = 1;
-                if (leftPressed && !rightPressed)   direction.x = -1;
+                if (rightPressed && !leftPressed) direction.x = 1;
+                if (leftPressed && !rightPressed) direction.x = -1;
                 break;
             case 'y':
-                if (upPressed)    direction.y =  1;
+                if (upPressed) direction.y =  1;
                 break;
         }
+        
         return direction;
     }
-    
-
-
-    /*
-    private void MovementControl()
-    {
-
-        Vector3 direction = Vector3.zero;   
-
-        // Control del movimiento del jugador en el eje X (izquierda a derecha)
-        if (Keyboard.current.leftArrowKey.isPressed)  
-                direction.x = -1;
-        if (Keyboard.current.rightArrowKey.isPressed) 
-                direction.x = 1;
-       
-        if (direction.x!=0)
-            lastDirection= direction;
-            
-        if(direction.x==0 && !Keyboard.current.shiftKey.isPressed)
-            currSpeed=currSpeed*drag;
-        
-        //currSpeed=Mathf.Clamp(value:currSpeed,min:-maxSpeed,maxSpeed);
-        transform.Translate(lastDirection.normalized * currSpeed *Time.deltaTime);
-        if (direction.x==0 && currSpeed<0.1f) //Hay que guardar la direccion que llevaba, porque cuando dejan de pulsar se vuelve 0 y no se moveria
-            lastDirection= Vector3.zero;
-
-
-    }   
-    
-
-    private void JumpControl()
-    {
-        // Control del salto del jugador en el eje Y
-        if (Keyboard.current.upArrowKey.isPressed) 
-        {
-            // Este if se asegura que solo podamos pulsar la tecla de salto una vez hasta que caiga al suelo
-            // sin importar la altura de la superficie
-            if (rigidbody.velocity.y == 0)
-                rigidbody.AddForce(
-                    Vector3.up * jumpForce * Time.fixedDeltaTime, 
-                    ForceMode.Impulse
-                );
-        }
-    }
-
-    private void accelerationControl()
-    {
-        // Control de la aceleración al pulsar la tecla Shift
-        var leftPressed=Keyboard.current.leftArrowKey.isPressed;
-        var rightPressed=Keyboard.current.rightArrowKey.isPressed;
-        if ((leftPressed || rightPressed) && Keyboard.current.shiftKey.isPressed)
-        {
-            if (currSpeed < maxSpeed)
-                currSpeed += acceleration * Time.deltaTime;
-        }
-        if ((leftPressed || rightPressed) && !Keyboard.current.shiftKey.isPressed) 
-            currSpeed = speed;
-    }
-    */
 }
