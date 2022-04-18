65aaf8beec47fdf1e5092a7fd91f264dd88ac22a
Estam314da <laura.cg.36@hotmail.com>
2022-04-18 20:11:23 +0200

    ultimo commit restaurado: limpieza de comentarios, añadida variable directionDrag y añadido control de salto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     public float maxSpeed;
     public float acceleration;
     public float drag;
+    public float directionDrag;
     private float currSpeed;
 
     private Vector3 lastDirection;
@@ -18,6 +19,10 @@ public class Player : MonoBehaviour
     [Header("Salto")]
     public float jumpForce;
     public float gravity;
+
+    public float liftingSpeed;
+    public float fallingSpeed;
+
 
     private new Rigidbody rigidbody;
 
@@ -32,6 +37,7 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
+        JumpControl();
         CustomGravity();
     }
 
@@ -43,121 +49,98 @@ public class Player : MonoBehaviour
     private void MovementControl()
     {
         Vector3 direction = GetDirection('x');
-        //Vector3 direction = GetDirection();
+
         bool shiftKey = Keyboard.current.shiftKey.isPressed;
 
-        // Incercia
         if (direction.x != 0)
         {
-            // Aceleración
             if (shiftKey)
             {
+                // Aceleración
                 if (currSpeed < maxSpeed)
                     currSpeed += acceleration * Time.deltaTime;
             }
-            else 
+            else
                 currSpeed = speed;
 
-
-            // Inercia al cambiar de dirección
+            // Inercia al cambio de dirección
             if (direction != lastDirection)
-                currSpeed *= 0.5f; 
+                currSpeed *= directionDrag; 
 
             lastDirection = direction;
         }
-        // Inercia al detenerse
         else 
+            // Inercia al detenerse
             currSpeed *= drag;
     
         transform.Translate(lastDirection * currSpeed * Time.deltaTime);
     }
 
-    private void AccelerationControl()
-    {
-
-    }
-
     private void JumpControl()
     {
+        Vector3 direction = GetDirection('y');
 
+        // Borra los comentarios que consideres innecesarios o cámbialos 👍
+        // Para la velocidad de caída y la velocidad de salto
+        float verticalForce = 1;
+        if (direction.y == 1)
+            verticalForce = liftingSpeed;
+        if (direction.y == -1)
+            verticalForce = fallingSpeed;
+
+        // El algoritmo que teníamos en un principio
+        if (rigidbody.velocity.y == 0)
+        {
+            rigidbody.AddForce(
+                direction * jumpForce * Time.fixedDeltaTime, 
+                ForceMode.Impulse
+            );
+        }
+        else
+        {
+            if (direction.y != 0)
+            {
+                // Si quitamos este condicional, la caída tendrá una resistencia al mentener pulsada
+                // la tecla up
+                if (rigidbody.velocity.y < 0 && direction.y > 0)
+                    return;
+
+                // ForceMode.Acceleration quien impulsa la velocidad de caída o salto (llegar más alto)
+                rigidbody.AddForce(
+                    direction * verticalForce * Time.fixedDeltaTime, ForceMode.Acceleration
+                );  
+            }
+        }
+
+            // if (direction.y == 1)
+            // {
+            //     if (rigidbody.velocity.y > 0)
+            //         rigidbody.AddForce(
+            //         Vector3.up * 1500 * Time.fixedDeltaTime, ForceMode.Acceleration
+            //     );
+            // }
     }
 
-    
     private Vector3 GetDirection(char axis)
     {
         Vector3 direction = Vector3.zero;
+
         bool rightPressed= Keyboard.current.rightArrowKey.isPressed;
         bool leftPressed= Keyboard.current.leftArrowKey.isPressed;
         bool upPressed= Keyboard.current.upArrowKey.isPressed;
+
         switch (axis)
         {
             case 'x':
-                if (rightPressed && !leftPressed)   direction.x = 1;
-                if (leftPressed && !rightPressed)   direction.x = -1;
+                if (rightPressed && !leftPressed) direction.x = 1;
+                if (leftPressed && !rightPressed) direction.x = -1;
                 break;
             case 'y':
-                if (upPressed)    direction.y =  1;
+                if (upPressed) direction.y =  1;
                 break;
         }
+        
         return direction;
     }
-    
 
-
-    /*
-    private void MovementControl()
-    {
-
-        Vector3 direction = Vector3.zero;   
-
-        // Control del movimiento del jugador en el eje X (izquierda a derecha)
-        if (Keyboard.current.leftArrowKey.isPressed)  
-                direction.x = -1;
-        if (Keyboard.current.rightArrowKey.isPressed) 
-                direction.x = 1;
-       
-        if (direction.x!=0)
-            lastDirection= direction;
-            
-        if(direction.x==0 && !Keyboard.current.shiftKey.isPressed)
-            currSpeed=currSpeed*drag;
-        
-        //currSpeed=Mathf.Clamp(value:currSpeed,min:-maxSpeed,maxSpeed);
-        transform.Translate(lastDirection.normalized * currSpeed *Time.deltaTime);
-        if (direction.x==0 && currSpeed<0.1f) //Hay que guardar la direccion que llevaba, porque cuando dejan de pulsar se vuelve 0 y no se moveria
-            lastDirection= Vector3.zero;
-
-
-    }   
-    
-
-    private void JumpControl()
-    {
-        // Control del salto del jugador en el eje Y
-        if (Keyboard.current.upArrowKey.isPressed) 
-        {
-            // Este if se asegura que solo podamos pulsar la tecla de salto una vez hasta que caiga al suelo
-            // sin importar la altura de la superficie
-            if (rigidbody.velocity.y == 0)
-                rigidbody.AddForce(
-                    Vector3.up * jumpForce * Time.fixedDeltaTime, 
-                    ForceMode.Impulse
-                );
-        }
-    }
-
-    private void accelerationControl()
-    {
-        // Control de la aceleración al pulsar la tecla Shift
-        var leftPressed=Keyboard.current.leftArrowKey.isPressed;
-        var rightPressed=Keyboard.current.rightArrowKey.isPressed;
-        if ((leftPressed || rightPressed) && Keyboard.current.shiftKey.isPressed)
-        {
-            if (currSpeed < maxSpeed)
-                currSpeed += acceleration * Time.deltaTime;
-        }
-        if ((leftPressed || rightPressed) && !Keyboard.current.shiftKey.isPressed) 
-            currSpeed = speed;
-    }
-    */
 }
